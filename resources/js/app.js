7c3fd6cb5fa683e0f3b5adf7ff25f748cb2a54f2
Alexandre <aletoscaro@gmail.com>
2024-05-08 10:31:13 -0300

    feat: adicionando o sistema laravel/breeze para a aplicação

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,23 @@
-import "./bootstrap";
-import { createApp } from "vue";
+import './bootstrap';
+import '../css/app.css';
 
-import App from "./App.vue";
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-createApp(App).mount("#app");
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-        vue(),
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: 'resources/js/app.js',
+            ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
-    resolve: {
-        alias: {
-            vue: "vue/dist/vue.esm-bundler.js",
-        },
-    },
 });
