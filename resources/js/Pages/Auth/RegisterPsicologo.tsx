aa77778af840a4ebefa68bd9c47263aaf33694b0
Alexandre Oliveira <aletoscaro@gmail.com>
2024-05-09 15:52:39 -0300

    feat: alterando alguns componentes para verificar funcionalidade nova do back

---
+++ resources/js/Pages/Auth/RegisterPsicologo.tsx
@@ -0,0 +1,14 @@
+import { useEffect, FormEventHandler } from 'react';
+import GuestLayout from '@/Layouts/GuestLayout';
+import InputError from '@/Components/InputError';
+import InputLabel from '@/Components/InputLabel';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextInput from '@/Components/TextInput';
+import { Head, Link, useForm } from '@inertiajs/react';
+import RegisterForm from '@/Components/RegisterForm';
+
+export default function Register() {
+    return (
+        <RegisterForm permissao='psicologo'/>
+    );
+}
--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -37,10 +37,16 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
                                             Log in
                                         </Link>
                                         <Link
-                                            href={route('register')}
+                                            href="/register-psicologo"
                                             className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                         >
-                                            Register
+                                            Register Psicologo
+                                        </Link>
+                                        <Link
+                                            href="/register-paciente"
+                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        >
+                                            Register Paciente
                                         </Link>
                                     </>
                                 )}
