089ea2967f232b0549cb022259c0789eefd6d0d5
Guto06 <agtomorato@gmail.com>
2024-05-15 20:33:14 -0300

    style: mudando espaçamento e cores

--- resources/js/Components/ApplicationLogo.tsx
+++ resources/js/Components/ApplicationLogo.tsx
@@ -3,6 +3,6 @@ import logo from "../assets/logo2.png"
 
 export default function ApplicationLogo(props: ImgHTMLAttributes<HTMLImageElement>) {
     return (
-        <img className="h-10 w-10 mr-2"src={logo} alt="logo" />
+        <img className="h-20 w-20 mr-2"src={logo} alt="logo" />
     );
 }
