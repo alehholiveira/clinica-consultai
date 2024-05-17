b96b8c136d3f8f767b06d194d20fee27f1c3a9af
Guto06 <agtomorato@gmail.com>
2024-05-16 22:26:33 -0300

    style: cor da landpage

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -181,7 +181,7 @@ export default function Welcome({
                     </div>
                 </div>
             </div>
-            <div className=" mt-20 border-b border-neutral-800 min-h-[800px]">
+            <div className=" mt-20 border-b border-transparent">
                 <div className="text-center">
                     <span className="bg-white text-indigo-500  border border-indigo-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase ">
                         NOSSA EQUIPE
@@ -194,11 +194,11 @@ export default function Welcome({
                         </span>
                     </h2>
                 </div>
-                <div className="flex flex-wrap mt-10 lg:mt-20 ">
+                <div className="flex flex-wrap mt-10 lg:mt-20 justify-center items-center ">
                     {equipe.map((equipe) => (
                         <div
                             key={equipe.text}
-                            className="w-full sm:1/2 lg:w-1/3"
+                            className="ml-5 mr-5 w-full sm:1/3 lg:w-1/3 "
                         >
                             <div className="flex">
                                 <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-indigo-500 justify-center items-center rounded-full"></div>
@@ -215,15 +215,15 @@ export default function Welcome({
                     ))}
                 </div>
             </div>
-            <div className="mt-10 tracking-wide">
+            <div className=" tracking-wide bg">
                 <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-10 lg:my-20">
-                    Clientes
+                    Depoimentos
                 </h2>
                 <div className="flex flex-wrap justify-center">
                     {Clientes.map((Clientes) => (
                         <div
                             key={Clientes.text}
-                            className="w-full sm:w-1/6 lg:w-1/8 px-1 py-2"
+                            className=" ml-5 mr-5 w-full sm:w-1/6 lg:w-1/3 px-1 py-2"
                         >
                             <div className="bg-white rounded-md p-6 text-md border-2 border-indigo-500 font-thin h-full flex flex-col justify-between">
                                 <p>{Clientes.text}</p>
