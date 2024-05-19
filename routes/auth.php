c81798a139f776f9bae65f122d9cd896a3ac374d
Vinicius Galassi <viniciusgalassi@gmail.com>
2024-05-18 22:41:40 -0300

    feat: criação de sistema de envio de formulário por e-mail

--- routes/auth.php
+++ routes/auth.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\MailController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\Auth\AppointmentController;
@@ -31,6 +32,9 @@ Route::middleware('guest')->group(function () {
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
         ->name('password.store');
+
+    Route::post('/send',[MailController::class,'send'])->name('mail-send');
+
 });
 
 Route::middleware('auth')->group(function () {
