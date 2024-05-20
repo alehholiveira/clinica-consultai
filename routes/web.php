639af84c3af26f3b86ce5558dc71ac745e7588b7
Alexandre Oliveira <aletoscaro@gmail.com>
2024-05-20 12:23:00 -0300

    feat: adicionando funcionalidade de editar e deletar informações cadastrais do paciente e criando reespectivas rotas

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ Route::get('/', function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile/{patient_id}', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile/{patient_id}', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile/{patient_id}', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
