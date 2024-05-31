99e5a711896da54b183fb0c6da6b070f2927f90c
Alexandre Oliveira <aletoscaro@gmail.com>
2024-05-31 14:09:14 -0300

    fix: corrigindo notificação de chegada de paciente para psicologos que não tem consulta agendado com o mesmo

--- resources/js/Components/GetNotification.tsx
+++ resources/js/Components/GetNotification.tsx
@@ -4,8 +4,9 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale/pt-BR';
+import { Appointment } from '@/types';
 
-export const GetNotification = () => {
+export const GetNotification = ({ consultas }: { consultas: Appointment[] }) => {
     const formatDateTime = (dateString: string, timeString: string) => {
         const date = new Date(`${dateString}T${timeString}`);
         return format(date, "'do dia' dd/MM/yyyy 'às' HH:mm", { locale: ptBR });
@@ -19,17 +20,25 @@ export const GetNotification = () => {
 
         // Escuta pelo evento .my-event
         channel.listen('.my-event', function (data: any) {
-            toast.success(`O paciente chegou para a consulta ${formatDateTime(data.appointment.date, data.appointment.time)}!`, {
-                position: "top-right",
-                autoClose: 13000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-            });
+            const psychologistId = data.appointment.psychologist.id;
+
+            // Verifica se o ID do psicólogo(que veio da notificação) está presente em alguma das consultas(do psicologo logado)
+            const consultaEncontrada = consultas.some((consulta) => consulta.psychologist_id === psychologistId);
+
+            if (consultaEncontrada) {
+                toast.success(`${data.appointment.patient.name} chegou para a consulta ${formatDateTime(data.appointment.date, data.appointment.time)}!`, {
+                    position: "top-right",
+                    autoClose: 13000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+            }
         });
+
 
         // Cleanup
         return () => {
--- resources/js/Pages/DashboardPsicologo.tsx
+++ resources/js/Pages/DashboardPsicologo.tsx
@@ -43,7 +43,7 @@ export default function Dashboard({ auth, pacientes, consultas }: PageProps<{ pa
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            <GetNotification/>
+                            <GetNotification consultas={consultas}/>
                             Dashboard psicologo
                             {Object.values(pacientes).map((paciente) => (
                                 <div key={paciente.id}>
