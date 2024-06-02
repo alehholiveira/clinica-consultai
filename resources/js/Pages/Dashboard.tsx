801a940c5cec241015fea50bb9cfb0a5031c5b5b
Alexandre Oliveira <aletoscaro@gmail.com>
2024-06-02 14:14:42 -0300

    style: alterando header do pagina da secretária

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -43,9 +43,9 @@ export default function Dashboard({ auth, ConsultasNoDiaDeHoje }: PageProps<{ Co
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Área da secretária</h2>}
         >
-            <Head title="Dashboard" />
+            <Head title="Área da secretária" />
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
