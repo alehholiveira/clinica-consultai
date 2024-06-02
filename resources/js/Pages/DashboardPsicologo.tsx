77c72c39d023acd7d46286bfa37c37410dbb6f0b
Alexandre Oliveira <aletoscaro@gmail.com>
2024-06-02 15:01:25 -0300

    style: adicionando estilo para a dashboard de psicologo

--- resources/js/Pages/DashboardPsicologo.tsx
+++ resources/js/Pages/DashboardPsicologo.tsx
@@ -2,12 +2,10 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head, Link, useForm } from '@inertiajs/react';
 import { Appointment, PageProps, User } from '@/types';
 import { FormEventHandler, useEffect } from 'react';
-import axios from 'axios';
 import PrimaryButton from '@/Components/PrimaryButton';
 import { GetNotification } from './../Components/GetNotification';
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale/pt-BR';
-
 
 export default function Dashboard({ auth, pacientes, consultas }: PageProps<{ pacientes: User[], consultas: Appointment[] }>) {
     const { data, setData, post, processing, errors, reset } = useForm({
@@ -32,58 +30,59 @@ export default function Dashboard({ auth, pacientes, consultas }: PageProps<{ pa
         }
     }, [data]);
 
-
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Área do psicólogo</h2>}
         >
-            <Head title="Dashboard" />
+            <Head title="Área do psicólogo" />
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            <GetNotification consultas={consultas}/>
-                            Dashboard psicologo
-                            {Object.values(pacientes).map((paciente) => (
-                                <div key={paciente.id}>
-                                    <h3>{paciente.name} -
-                                        <PrimaryButton className="ms-4 h-8" disabled={processing} >
-                                            <Link
-                                                href={`/profile/${paciente.id}`}
-                                                className="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
-                                            >
-                                                informacões do paciente
-                                            </Link>
-                                        </PrimaryButton>
-                                    </h3>
-                                    <p>
-                                        consultas:{" "}
-                                        {consultas
-                                            .filter((consulta) => consulta.patient_id === paciente.id)
-                                            .map((consulta) => (
-                                                <span key={consulta.id}>
-                                                    <br />{formatDateTime(consulta.date, consulta.time)} -
-
-                                                    {!consulta.hasMeetingSession && (
-
-                                                        <button onClick={submit(String(consulta.id))}>
-                                                            Criar MeetingSession
-                                                        </button>
-                                                    )}
-                                                    {consulta.hasMeetingSession && (
-                                                        <Link
-                                                            href={`/appointment/${paciente.name}/${consulta.id}`}
-                                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
-                                                        >
-                                                            informacao da sessao
-                                                        </Link>
-                                                    )}
-                                                </span>
-                                            ))}
-                                    </p>
-                                </div>
-                            ))}
+                            <GetNotification consultas={consultas} />
+                            <div className="space-y-6">
+                                {Object.values(pacientes).map((paciente) => (
+                                    <div key={paciente.id} className="bg-gray-100 p-4 rounded-lg shadow-md">
+                                        <div className="flex justify-between items-center mb-4">
+                                            <h3 className="text-lg font-semibold">{paciente.name}</h3>
+                                            <PrimaryButton className="bg-gradient-to-r from-indigo-500 to-sky-500 py-2 px-3 border-white text-white rounded-md" disabled={processing} >
+                                                <a href={`/profile/${paciente.id}`}>
+                                                    Informações do paciente
+                                                </a>
+                                            </PrimaryButton>
+                                        </div>
+                                        <p className="text-sm text-gray-600">
+                                            Consultas:
+                                            {consultas
+                                                .filter((consulta) => consulta.patient_id === paciente.id)
+                                                .map((consulta) => (
+                                                    <div key={consulta.id} className="mt-2">
+                                                        <span className="block">
+                                                            {formatDateTime(consulta.date, consulta.time)} -
+                                                            {!consulta.hasMeetingSession && (
+                                                                <button
+                                                                    className="ml-2 text-blue-500 hover:underline"
+                                                                    onClick={submit(String(consulta.id))}
+                                                                >
+                                                                    Criar Informações da Sessão
+                                                                </button>
+                                                            )}
+                                                            {consulta.hasMeetingSession && (
+                                                                <Link
+                                                                    href={`/appointment/${paciente.name}/${consulta.id}`}
+                                                                    className="ml-2 text-blue-500 hover:underline"
+                                                                >
+                                                                    Informação da Sessão
+                                                                </Link>
+                                                            )}
+                                                        </span>
+                                                    </div>
+                                                ))}
+                                        </p>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
